896297e72092c3b8994694b00ace15aceeea977c
aahana <89208022+uditgirdhar@users.noreply.github.com>
2021-08-24 08:56:30 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,8 +7,23 @@ var b = 0;
 
 function setup() {
   createCanvas(400, 400);  
+  btn_red=createButton("red");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+  btn_green=createButton("green");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
 }
-
+function red_bg(){
+  r=255
+  b=0
+  g=0
+}
+function green_bg(){
+  r=0
+  b=0
+  g=255
+}
 function draw() {
   background(r,g,b);
 }
